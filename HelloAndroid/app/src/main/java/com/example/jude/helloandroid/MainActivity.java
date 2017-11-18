cb7f1d4b57df05df3f2f4bef18609e1c530889c7
Alphanor <orizonff@gmail.com>
2017-11-18 19:52:34 +0100

    Added alternative layouts for different screens

--- HelloAndroid/app/src/main/java/com/example/jude/helloandroid/MainActivity.java
+++ HelloAndroid/app/src/main/java/com/example/jude/helloandroid/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.jude.helloandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        Button openButton = (Button) findViewById(R.id.openButton);
+
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.udacity.com/"));
+                startActivity(intent);
+            }
+        });
     }
 }
