c62518a03bd95cceaa7116a2b82f7198358054fa
Alphanor <orizonff@gmail.com>
2017-12-01 17:46:20 +0100

    Added onSaveInstanceState and onRestoreInstanceState. Added a layout for landscape.

--- Court Counter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ Court Counter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.android.courtcounter;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,39 +16,66 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+    int scoreTeamA=0;
+    int scoreTeamB=0;
     ArrayList lastAddA = new ArrayList();
     ArrayList lastAddB = new ArrayList();
+    TextView scoreViewTeamA;
+    TextView scoreViewTeamB;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoreViewTeamA = (TextView) findViewById(R.id.team_a_score);
+        scoreViewTeamB = (TextView) findViewById(R.id.team_b_score);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        String scoreString = scoreViewTeamA.getText().toString();
+        savedInstanceState.putString("scoreViewTeamA", scoreString);
+
+        scoreString = scoreViewTeamB.getText().toString();
+        savedInstanceState.putString("scoreViewTeamB", scoreString);
+
+        savedInstanceState.putInt("scoreTeamA", scoreTeamA);
+        savedInstanceState.putInt("scoreTeamB", scoreTeamB);
+
+        savedInstanceState.putIntegerArrayList("lastAddA", lastAddA);
+
+        savedInstanceState.putIntegerArrayList("lastAddB", lastAddB);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        scoreViewTeamA.setText(savedInstanceState.getString("scoreViewTeamA"));
+        scoreViewTeamB.setText(savedInstanceState.getString("scoreViewTeamB"));
+        scoreTeamA = savedInstanceState.getInt("scoreTeamA");
+        scoreTeamB = savedInstanceState.getInt("scoreTeamB");
+
+        lastAddA = savedInstanceState.getIntegerArrayList("lastAddA");
+        lastAddB = savedInstanceState.getIntegerArrayList("lastAddB");
     }
 
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewTeamA.setText(String.valueOf(score));
     }
 
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewTeamB.setText(String.valueOf(score));
     }
 
     public void addThreeforTeamA(View v) {
         scoreTeamA += 3;
         lastAddA.add(3);
         displayForTeamA(scoreTeamA);
-    }
-
-    public void undoLastTeamA(View v) {
-        if (scoreTeamA>0) {
-            scoreTeamA -= (int) lastAddA.remove(lastAddA.size()-1);
-            displayForTeamA(scoreTeamA);
-        }
     }
 
     public void addTwoforTeamA(View v) {
@@ -60,6 +88,13 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA += 1;
         lastAddA.add(1);
         displayForTeamA(scoreTeamA);
+    }
+
+    public void undoLastTeamA(View v) {
+        if (scoreTeamA>0) {
+            scoreTeamA -= (int) lastAddA.remove(lastAddA.size()-1);
+            displayForTeamA(scoreTeamA);
+        }
     }
 
     public void addThreeforTeamB(View v) {
@@ -99,40 +134,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void scoreComparation(View v) {
 
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+
         if (scoreTeamA == scoreTeamB) {
             Toast.makeText(this, "The match is over!", Toast.LENGTH_SHORT).show();
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
             mBuilder.setSmallIcon(R.mipmap.ic_launcher);
             mBuilder.setContentTitle("Same result!");
             mBuilder.setContentText("Team A and Team B have "+scoreTeamA+" points.");
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(0, mBuilder.build());
+            notificationManager.notify(100, mBuilder.build());
 
         }
 
         else if (scoreTeamA > scoreTeamB) {
 
             Toast.makeText(this, "The match is over!", Toast.LENGTH_SHORT).show();
-
-            NotificationCompat.Builder mBuilder2 = new NotificationCompat.Builder(this);
-                mBuilder2.setSmallIcon(R.mipmap.ic_launcher);
-                mBuilder2.setContentTitle("Congratulations!");
-                mBuilder2.setContentText("Team A win the match with " + scoreTeamA + " points.");
+            mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+            mBuilder.setContentTitle("Congratulations!");
+            mBuilder.setContentText("Team A win the match with " + scoreTeamA + " points.");
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(0, mBuilder2.build());
+            notificationManager.notify(101, mBuilder.build());
 
         }
 
-        else if(scoreTeamA<scoreTeamB){
+        else {
 
             Toast.makeText(this, "The match is over!", Toast.LENGTH_SHORT).show();
-
-            NotificationCompat.Builder mBuilder3 = new NotificationCompat.Builder(this);
-            mBuilder3.setSmallIcon(R.mipmap.ic_launcher);
-            mBuilder3.setContentTitle("Congratulations!");
-            mBuilder3.setContentText("Team B win the match with " + scoreTeamB + " points.");
+            mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+            mBuilder.setContentTitle("Congratulations!");
+            mBuilder.setContentText("Team B win the match with " + scoreTeamB + " points.");
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(0, mBuilder3.build());
+            notificationManager.notify(102, mBuilder.build());
         }
 
     }
